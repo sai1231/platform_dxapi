02023366a35f14d50e51c0e08bf9f87a37f64147
Bavirisetty <Sai.Bavirisetty@in.pega.com>
2020-08-10 11:52:39 +0530

    endpoint

--- src/_services/endpoints.js
+++ src/_services/endpoints.js
@@ -1,6 +1,6 @@
 export const endpoints = {
   // Change this URL if you want to point the React application at another Pega server.
-  BASEURL: "https://e2eplatform84-dev.pegatsdemo.com/prweb/api/v1",
+  BASEURL: "/prweb/api/v1",
 
   AUTH: "/authenticate",
   CASES: "/cases",
